fe1b9b4c838008d03c8d8e27214e0833c9ca325b
kawhi.liu <kawhi.liu@theronts.biz>
2023-04-03 15:03:14 +0800

    F - Optimize the format of the details page content. - kawhi.liu

--- src/main/java/com/tool/pomodoro/technique/tool/controller/controller/today/TodayDetailController.java
+++ src/main/java/com/tool/pomodoro/technique/tool/controller/controller/today/TodayDetailController.java
@@ -1,8 +1,10 @@
 package com.tool.pomodoro.technique.tool.controller.controller.today;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.tool.pomodoro.technique.tool.controller.controller.today.vo.TodayVo;
 import com.tool.pomodoro.technique.tool.controller.controller.tool.ToolController;
 import javafx.fxml.FXML;
@@ -10,16 +12,19 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.ResourceBundle;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class TodayDetailController implements Initializable {
 
     private final static ObjectMapper objectMapper = new ObjectMapper();
+
     static {
-        objectMapper.registerModule(new JavaTimeModule());
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        objectMapper.registerModule(javaTimeModule);
     }
 
     @FXML
@@ -33,16 +38,27 @@ public class TodayDetailController implements Initializable {
 
     public void display(TodayVo todayVo) {
         Optional.ofNullable(todayVo)
-                .ifPresent(vo -> {
-                    try {
-                        todayDetailTextArea.setText(objectMapper.writeValueAsString(vo));
-                    } catch (JsonProcessingException e) {
-                        e.printStackTrace();
-                    }
-                });
+                .ifPresent(vo -> todayDetailTextArea.setText(format(todayVo)));
     }
 
-    public void displayNone() {
-        todayDetailTextArea.setText("");
+    private String format(TodayVo todayVo) {
+        StringBuilder build = new StringBuilder();
+        try {
+            JsonNode jsonNode = objectMapper.readTree(objectMapper.writeValueAsString(todayVo));
+            appendIfPresent("id", jsonNode, build);
+            appendIfPresent("content", jsonNode, build);
+            appendIfPresent("clocks", jsonNode, build);
+            appendIfPresent("category", jsonNode, build);
+            appendIfPresent("createTime", jsonNode, build);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return build.toString();
+    }
+
+    private void appendIfPresent(String fieldName, JsonNode jsonNode, StringBuilder builder) {
+        Optional.ofNullable(jsonNode.get(fieldName))
+                .map(JsonNode::asText)
+                .ifPresent(val -> builder.append(fieldName).append("=").append(val).append("\r\n"));
     }
 }
--- src/main/java/com/tool/pomodoro/technique/tool/controller/controller/todo/TodoDetailController.java
+++ src/main/java/com/tool/pomodoro/technique/tool/controller/controller/todo/TodoDetailController.java
@@ -1,8 +1,10 @@
 package com.tool.pomodoro.technique.tool.controller.controller.todo;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.tool.pomodoro.technique.tool.controller.controller.todo.vo.TodoVo;
 import com.tool.pomodoro.technique.tool.controller.controller.tool.ToolController;
 import javafx.fxml.FXML;
@@ -10,14 +12,19 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class TodoDetailController implements Initializable {
 
     private final static ObjectMapper objectMapper = new ObjectMapper();
+
     static {
-        objectMapper.registerModule(new JavaTimeModule());
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        objectMapper.registerModule(javaTimeModule);
     }
 
     @FXML
@@ -30,23 +37,25 @@ public class TodoDetailController implements Initializable {
 
     public void display(TodoVo todoVo) {
         Optional.ofNullable(todoVo)
-                .ifPresent(vo -> {
-                    try {
-                        String replace = objectMapper.writeValueAsString(vo)
-                                .replace("{", "")
-                                .replace("}", "")
-                                .replace("\"", "")
-                                .replace(":", "=");
-                        String[] dataList = replace.split(",");
-                        String data = String.join("\r\n", dataList);
-                        todoDetailTextAreal.setText(data);
-                    } catch (JsonProcessingException e) {
-                        e.printStackTrace();
-                    }
-                });
+                .ifPresent(vo -> todoDetailTextAreal.setText(format(vo)));
     }
 
-    public void displayNone() {
-        todoDetailTextAreal.setText("");
+    private String format(TodoVo todoVo) {
+        StringBuilder build = new StringBuilder();
+        try {
+            JsonNode jsonNode = objectMapper.readTree(objectMapper.writeValueAsString(todoVo));
+            appendIfPresent("id", jsonNode, build);
+            appendIfPresent("content", jsonNode, build);
+            appendIfPresent("createTime", jsonNode, build);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return build.toString();
+    }
+
+    private void appendIfPresent(String fieldName, JsonNode jsonNode, StringBuilder builder) {
+        Optional.ofNullable(jsonNode.get(fieldName))
+                .map(JsonNode::asText)
+                .ifPresent(val -> builder.append(fieldName).append("=").append(val).append("\r\n"));
     }
 }
