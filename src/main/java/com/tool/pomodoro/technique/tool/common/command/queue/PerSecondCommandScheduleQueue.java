ad7732dfcf6ac3a8b6ef91ae877622b7fa1e0d1b
kawhi.liu <kawhi.liu@theronts.biz>
2023-04-07 15:54:48 +0800

    F - Make the countdown time more precise. - kawhi.liu

--- src/main/java/com/tool/pomodoro/technique/tool/common/command/queue/PerSecondCommandScheduleQueue.java
+++ src/main/java/com/tool/pomodoro/technique/tool/common/command/queue/PerSecondCommandScheduleQueue.java
@@ -3,7 +3,11 @@ package com.tool.pomodoro.technique.tool.common.command.queue;
 import com.tool.pomodoro.technique.tool.common.command.Command;
 import com.tool.pomodoro.technique.tool.common.init.ToolInit;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoField;
 import java.util.LinkedList;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Queue;
 
@@ -45,6 +49,7 @@ public class PerSecondCommandScheduleQueue implements CommandScheduleQueue, Tool
 
     private static class SleepCommand implements Command {
         private final long sleepTime;
+        private LocalDateTime lastWokeUp;
 
         public SleepCommand(long sleepTime) {
             this.sleepTime = sleepTime;
@@ -52,12 +57,33 @@ public class PerSecondCommandScheduleQueue implements CommandScheduleQueue, Tool
 
         @Override
         public void execute() {
+            long thisSleepTime = calculateThisSleepTime();
+            if (thisSleepTime > 0) {
+                sleep(thisSleepTime);
+            }
+            PerSecondCommandScheduleQueue.getInstance().put(this);
+        }
+
+        private long calculateThisSleepTime() {
+            long thisSleepTime;
+            if (Objects.isNull(lastWokeUp)) {
+                thisSleepTime = sleepTime;
+            } else {
+                int lastWokeUpMilli = lastWokeUp.get(ChronoField.MILLI_OF_DAY);
+                int nowMilli = LocalDateTime.now().get(ChronoField.MILLI_OF_DAY);
+                thisSleepTime = sleepTime - (nowMilli - lastWokeUpMilli);
+            }
+            return thisSleepTime;
+        }
+
+        private void sleep(long sleepTime) {
             try {
                 Thread.sleep(sleepTime);
-                PerSecondCommandScheduleQueue.getInstance().put(this);
+                lastWokeUp = LocalDateTime.now();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+
     }
 }
